c23a140f3992603f16f91b4b3fa796b8dc8948fe
Diego Mendoza <diegomf.mendoza@gmail.com>
2019-03-18 15:00:59 -0500

    first commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-messaging.js');
+
+  // Initialize Firebase
+  var config = {
+    messagingSenderId: "110859357842"
+  };
+  firebase.initializeApp(config);
+
+  const messaging = firebase.messaging();
+  messaging.setBackgroundMessageHandler(function( payload){
+  //   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  //   // // Customize notification here
+    var notificationTitle = 'Background Message Title';
+    var notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+    console.log(payload);
+    return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+    // return self.registration.showNotification('HOLA',{body:"como"});
+  });
